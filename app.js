e94eff02518c58d68520cc84b76c47f674a56710
Violet Stewart <vkazoo2@gmail.com>
2021-09-29 11:04:46 -0700

    learned about arrays

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// import functions and grab DOM elements
+//strings
+console.log('this is what screaming in gorgeous sounds like');
+//numbers
+console.log(1*5);
+//booleans
+console.log(true);
+console.log(false);
+//empty-ness
+console.log(undefined);
+console.log(null);
+//variables
+const PI = 3.14;
+console.log(PI);
+console.log(`The area of a circle with radius 2 is: ${PI * 2 * 2}`);
 
-// initialize global state
+// arrays
+let myList= [1, 2, 3, 4];
+console.log(myList);
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+let colours= ['pink', 'blue'];
+console.log(colours[0]);
+
+colours[1]= 'purple';
+console.log(colours);
+
